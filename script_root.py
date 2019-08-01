5210b2e9bb8ace960c93b2b08b33572c41f56c12
Raghadoliwi <52508523+Raghadoliwi@users.noreply.github.com>
2019-08-01 12:31:05 +0300

    Update script_root.py

--- script_root.py
+++ script_root.py
@@ -12,6 +12,11 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 
+silence_list = []
+
+	
+	
+
 def windows(signal, window_size, step_size):
     if type(window_size) is not int:
         raise AttributeError("Window size must be an integer.")
@@ -19,11 +24,11 @@ def windows(signal, window_size, step_size):
         raise AttributeError("Step size must be an integer.")
     min_w = 0
     max_w = 0
-    for i_start in range(0, len(signal), step_size): #array that is sequencing from 0 to len(signal) by step_size
+    for i_start in range(0, len(signal), step_size):
         i_end = i_start + window_size
         if i_end >= len(signal):
             break
-        yield signal[i_start:i_end] #similar to return 
+        yield signal[i_start:i_end]
 
 def energy(samples):
     return np.sum(np.power(samples, 2.)) / float(len(samples))
@@ -38,60 +43,55 @@ def edges(binary_signal):
             yield index
         previous_value = x
         index += 1
+def silences(sample ,duration , threshold):
+	silence_list.clear()
+	filename = sample
+	window_duration = float(duration)
+	step_duration = window_duration / 1.
+	silence_threshold = float(threshold)
 
-		
-
-
-'''
-# plot times for debugging
-import matplotlib.pyplot as plt
-x = [i/float(sample_rate) for i in range(len(samples))]
-plt.plot(x, samples)
-plt.plot(times, [0 for i in times], 'ro')
-plt.plot([i+window_duration for i in times], [0 for i in times], 'k.')
-plt.show()
-'''
-
-
-@app.route('/_find_silences')
-def findSilences():
-    #a = request.args.get('a', 0, type=int)
-   	#b = request.args.get('b', 0, type=int)
- output = ""
- path = request.args.get('path')
- filename = path
- window_duration = .1
- step_duration = window_duration / 1.
- silence_threshold = .0001
-
- sample_rate, samples = wavfile.read(filename, mmap=True)
+	sample_rate, samples = wavfile.read(filename, mmap=True)
 	#max_amplitude = max(samples)
 	#max_amplitude = np.iinfo(samples.dtype).max
 	#max_energy = energy([max_amplitude])
 
- window_size = int(window_duration * sample_rate)
- step_size = int(step_duration * sample_rate)
+	window_size = int(window_duration * sample_rate)
+	step_size = int(step_duration * sample_rate)
 
- signal_windows = windows(
-    signal=samples,
-    window_size=window_size,
-    step_size=step_size
-	)	
+	signal_windows = windows(
+		signal=samples,
+    	window_size=window_size,
+    	step_size=step_size
+	)
 
 	#window_energy = [energy(w) / max_energy for w in signal_windows]
- window_energy = [energy(w) for w in signal_windows]
- min_e = min(window_energy)
- max_e = max(window_energy)
+	window_energy = [energy(w) for w in signal_windows]
+	min_e = min(window_energy)
+	max_e = max(window_energy)
 
 	#window_silence = [e < silence_threshold for e in window_energy]
- window_silence = ((e-min_e)/(max_e-min_e) < silence_threshold for e in window_energy)
- times = [r*step_duration for r in edges(window_silence)]
+	window_silence = ((e-min_e)/(max_e-min_e) < silence_threshold for e in window_energy)
+	times = [r*step_duration for r in edges(window_silence)]
+	
+	for i in range(0, len(times), 2):
+		if i+1<len(times):
+			print(times[i], times[i+1])
+			silence_list.append(times[i])
+			silence_list.append(times[i+1]) 
+		else:
+			print(times[i], len(samples)/float(sample_rate))
+			silence_list.append(times[i])
+			silence_list.append(len(samples)/float(sample_rate)) 
+			
+		
 
- for i in range(0, len(times), 2):
-            if(i+1<len(times)): output+= times[i] +""+ times[i+1]
-            else: output+= times[i] +""+ len(samples)/float(sample_rate)
- return jsonify(silences = output)
-'''
+
+@app.route('/_find_silences')
+def add_numbers():
+	a = request.args.get('path', None, type=str)
+	silences(a,.1,.0001)
+	return jsonify(result=silence_list)
+
 @app.route('/')
 def index():
     return render_template('layouts/transcription.html')
@@ -101,4 +101,13 @@ def index():
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port = 5000)
+
+'''
+# plot times for debugging
+import matplotlib.pyplot as plt
+x = [i/float(sample_rate) for i in range(len(samples))]
+plt.plot(x, samples)
+plt.plot(times, [0 for i in times], 'ro')
+plt.plot([i+window_duration for i in times], [0 for i in times], 'k.')
+plt.show()
 '''
